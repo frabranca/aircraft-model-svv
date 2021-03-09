baa3e0ce474bddcfded12cec96a9df6ff0258256
noutb <noutbuijs@hotmail.com>
2021-03-09 10:52:35 +0100

    changes

--- analytical_analysis.py
+++ analytical_analysis.py
@@ -1,5 +1,34 @@
 import analytical_main as am
 import numpy as np
 import control.matlab as ml
+from matplotlib import pyplot as plt
 
-if __name__ == "__main__"
+if __name__ == "__main__":
+    ac = am.ac(hp0=5288.28)
+    V = 102.88889
+    t = np.linspace(0,25,250)
+    X0 = np.array([0,np.radians(15),0,0])
+
+    sys1 = ac.asym(V)
+    y, t = ml.initial(sys1,t,X0)
+    print(y[0,:])
+
+    plt.subplot(221)
+    plt.ylim(-)
+    plt.plot(t, np.degrees(y[:,0]))
+    plt.grid()
+
+    plt.subplot(222)
+    plt.plot(t, np.degrees(y[:,1]))
+    plt.grid()
+
+    plt.subplot(223)
+    plt.plot(t, np.degrees(y[:,2]))
+    plt.grid()
+
+    plt.subplot(224)
+    plt.plot(t, np.degrees(y[:,3]))
+    plt.grid()
+
+    plt.show()
+
