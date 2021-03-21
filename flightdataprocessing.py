f3f2384d79299157589457bd62f289dc028681de
noutb <noutbuijs@hotmail.com>
2021-03-21 15:31:35 +0100

    sdasfaf

--- flightdataprocessing.py
+++ flightdataprocessing.py
@@ -54,8 +54,13 @@ def approx(t, x, step=35):
     xx = xx.reshape(int(np.size(xx)/step), step)
     tt = tt.reshape(int(np.size(tt)/step), step)
 
-    xx = np.average(xx, axis=1)
-    tt = np.average(tt, axis=1)
+    xx_avg = np.average(xx, axis=1)
+    tt_avg = np.average(tt, axis=1)
+
+    xx = (np.ones((np.shape(xx))).T * xx_avg).T
+    tt = (np.ones((np.shape(tt))).T * tt_avg).T
+    xx = np.concatenate(xx)
+    tt = np.concatenate(tt)
 
     return tt, xx
 
@@ -64,13 +69,13 @@ def symplot(mode):
     end = mode[1]
 
     # time
-    tplot = t[(t > start) & (t < end)] #s
+    tplot = t[(t >= start) & (t <= end)] #s
     # velocity
-    vt = tas[(t > start) & (t < end)] #m/s
+    vt = tas[(t >= start) & (t <= end)] #m/s
 
     # initial conditions
     c = 2.0569
-    ind = np.where(t == tplot[0])[0]-1
+    ind = np.where(t == tplot[0])[0]
     m0 = m[ind] # kg
     h0 = hp[ind] # m
     V0 = tas[ind] #m/s
@@ -78,61 +83,18 @@ def symplot(mode):
     u0 = (vt[0]-V0)/V0 # -
     a0 = a[ind] #rad
     th0 = theta[ind] #rad
-    q0 = q[ind]*c/V0 #rad/s
+    q0 = q[ind]*c/V0 #rad
 
     # state variables
     uplot = (vt - V0)/V0 # -
-    aplot = a[(t > start) & (t < end)]
-    thplot = theta[(t > start) & (t < end)]
-    qplot = q[(t > start) & (t < end)] * c/V0
-    deplot = de[(t > start) & (t < end)]
-
-    tplot = tplot/60
-    f=20
-
-    # plt.figure(figsize=(12,8))
-    # tt, uu = approx(tplot, uplot)
-    # plt.xlabel('Time [min]', fontsize=f)
-    # plt.ylabel('Normalized velocity change [-]', fontsize=f)
-    # #     plt.plot(tt, uu, 'k')
-    # plt.plot(tplot, uplot, 'c')
-    #
-    # tt, aa = approx(tplot, aplot)
-    # plt.figure(figsize=(12,8))
-    # plt.xlabel('Time [min]', fontsize=f)
-    # plt.ylabel('Angle of attack [rad]', fontsize=f)
-    # plt.plot(tt, aa, 'k', label='Approximated')
-    # plt.plot(tplot, aplot, 'c', label='Real')
-    # plt.legend(fontsize=f)
-    #
-    # tt, thth = approx(tplot, thplot)
-    # plt.figure(figsize=(12,8))
-    # plt.xlabel('Time [min]', fontsize=f)
-    # plt.ylabel('Pitch angle [rad]', fontsize=f)
-    # #     plt.plot(tt, thth, 'k')
-    # plt.plot(tplot, thplot, 'c')
-    #
-    # tt, qq = approx(tplot, qplot)
-    # plt.figure(figsize=(12,8))
-    # plt.xlabel('Time [min]', fontsize=f)
-    # plt.ylabel('Pitch rate [rad/s]', fontsize=f)
-    # plt.plot(tt, qq, 'k', label='Approximated')
-    # plt.plot(tplot, qplot, 'c', label='Real')
-    # plt.legend(fontsize=f)
-    #
-    #
-    # tt, dede = approx(tplot, deplot)
-    # plt.figure(figsize=(12,8))
-    # plt.xlabel('Time [min]', fontsize=f)
-    # plt.ylabel('Elevator deflection [rad]', fontsize=f)
-    # plt.plot(tt, dede, 'k', label='Approximated')
-    # plt.plot(tplot, deplot, 'c', label='Real')
-    # plt.legend(fontsize=f)
+    aplot = a[(t >= start) & (t <= end)]
+    thplot = theta[(t >= start) & (t <= end)]
+    qplot = q[(t >= start) & (t <= end)] * c/V0
+    deplot = de[(t >= start) & (t <= end)]
 
     x0 = np.array([m0, h0, V0, t0, u0, a0, th0, q0])
     return x0, tplot, uplot, aplot, thplot, qplot, deplot
 
-# symplot(phugoid)
 
 def asymplot(mode):
     sns.set_theme()
@@ -140,12 +102,12 @@ def asymplot(mode):
     end = mode[1]
 
     # time
-    tplot = t[(t > start) & (t < end)] # s
-    mplot = m[(t > start) & (t < end)] # kg
+    tplot = t[(t >= start) & (t <= end)] # s
+    mplot = m[(t >= start) & (t <= end)] # kg
 
     # initial conditions
     b = 15.911
-    ind = np.where(t == tplot[0])[0]-1
+    ind = np.where(t == tplot[0])[0]
     m0 = mplot[ind] # kg
     h0 = hp[ind] # m
     V0 = tas[ind] # m/s
@@ -155,62 +117,18 @@ def asymplot(mode):
     p0 = p[ind]*b/V0/2
     r0 = r[ind]*b/V0/2
 
-    tplot = tplot/60
-    f=20
-    # state variables
-    #     bplot =
-    phiplot = phi[(t > start) & (t < end)]
-    pplot = p[(t > start) & (t < end)] *b/V0/2
-    rplot = r[(t > start) & (t < end)] *b/V0/2
-    daplot = da[(t > start) & (t < end)]
-    drplot = dr[(t > start) & (t < end)]
+    tplot = tplot
 
-    # roll angle
-    # plt.figure(figsize=(12,8))
-    # tt, phi_approx = approx(tplot, phiplot)
-    # plt.xlabel('Time [min]', fontsize=f)
-    # plt.ylabel('Roll angle [rad]', fontsize=f)
-    # plt.plot(tplot, phiplot, 'c')
-    # plt.plot(tt, phi_approx, 'k')
-    #
-    # # roll rate
-    # tt, p_approx = approx(tplot, pplot, step = 50)
-    # plt.figure(figsize=(12,8))
-    # plt.xlabel('Time [min]', fontsize=f)
-    # plt.ylabel('Roll rate', fontsize=f)
-    # plt.plot(tplot, pplot, 'c')
-    # plt.plot(tt, p_approx, 'k')
-    #
-    # # yaw rate
-    # tt, r_approx = approx(tplot, rplot)
-    # plt.figure(figsize=(12,8))
-    # plt.xlabel('Time [min]', fontsize=f)
-    # plt.ylabel('Yaw rate', fontsize=f)
-    # plt.plot(tplot, rplot, 'c')
-    # plt.plot(tt, r_approx, 'k')
-    #
-    # # aileron deflection
-    # tt, da_approx = approx(tplot, daplot)
-    # plt.figure(figsize=(12,8))
-    # plt.xlabel('Time [min]', fontsize=f)
-    # plt.ylabel('Aileron deflection', fontsize=f)
-    # plt.plot(tplot, daplot, 'c')
-    # plt.plot(tt, da_approx, 'k')
-    #
-    # # rudder deflection
-    # tt, dr_approx = approx(tplot, drplot)
-    # plt.figure(figsize=(12,8))
-    # plt.xlabel('Time [min]', fontsize=f)
-    # plt.ylabel('Rudder deflection', fontsize=f)
-    # plt.plot(tplot, drplot, 'c')
-    # plt.plot(tt, dr_approx, 'k')
-
+    phiplot = phi[(t >= start) & (t <= end)]
+    pplot = p[(t >= start) & (t <= end)] *b/V0/2
+    rplot = r[(t >= start) & (t <= end)] *b/V0/2
+    daplot = da[(t >= start) & (t <= end)]
+    drplot = dr[(t >= start) & (t <= end)]
 
     x0 = np.array([m0, h0, V0, t0, beta0, phi0, p0, r0])
     return x0, tplot, phiplot, pplot, rplot
 
-# if __name__ == "__main__":
-short_data = symplot(short)
+# short_data = symplot(short)
 phugoid_data = symplot(phugoid)
 
 # dutch_data = asymplot(dutch)
@@ -220,12 +138,12 @@ phugoid_data = symplot(phugoid)
 # asymplot(spiral)
 
 if __name__ == "__main__":
-    from scipy import integrate
-    def f(x): return x*x
-    x = np.arange(0.,1.,0.01)
-
-    def beta(x, f):
-        b = []
-        for i in x:
-            b.append(integrate.quad(f, 0., i)[0])
-        return b
+    # from scipy import integrate
+    # def f(x): return x*x
+    # x = np.arange(0.,1.,0.01)
+    #
+    # def beta(x, f):
+    #     b = []
+    #     for i in x:
+    #         b.append(integrate.quad(f, 0., i)[0])
+    #     return b
--- validation.py
+++ validation.py
@@ -1,40 +1,45 @@
 import numpy as np
-from flightdataprocessing import short_data, phugoid_data
+from flightdataprocessing import phugoid_data, approx
 import Numerical_main as nm
 from matplotlib import pyplot as plt
+
+
 
 class mode():
     def __init__(self, data):
         self.cond, self.tplot, self.uplot, self.aplot, self.thplot, self.qplot, self.deplot = data
-        self.t = np.linspace(0, (self.tplot[-1] - self.tplot[0]) * 60,np.size(self.tplot))
+        self.t = np.linspace(0, (self.tplot[-1] - self.tplot[0]),np.size(self.tplot))
         self.m0 = self.cond[0][0]
         self.h0 = self.cond[1][0]
         self.V0 = self.cond[2][0]
         self.t0 = self.cond[3][0]
         self.u0 = self.cond[4][0]
-        self.a0 = self.cond[4][0]
-        self.th0 = self.cond[5][0]
-        self.q0 = self.cond[6][0]
+        self.a0 = self.cond[5][0]
+        self.th0 = self.cond[6][0]
+        self.q0 = self.cond[7][0]
         self.x0 = np.array([self.u0, self.a0, self.th0, self.q0])
         self.totalplot = np.zeros((4,np.size(self.tplot)))
         self.totalplot[0,:] = self.uplot
         self.totalplot[1,:] = self.aplot
         self.totalplot[2,:] = self.thplot
         self.totalplot[3,:] = self.qplot
-
-
-# short = mode(short_data)
+        print(self.a0, self.aplot[0])
 phugoid = mode(phugoid_data)
+
+tapprox, deapprox = approx(phugoid.tplot, phugoid.deplot)
+
 ac_phugoid = nm.ac(m = phugoid.m0, initial = phugoid.x0, hp0 = phugoid.h0, V0 = phugoid.V0)
 y = ac_phugoid.sym_input_response(phugoid.t, phugoid.deplot, phugoid.x0)
 plt.subplot(121)
 plt.title("numerical model")
-plt.plot(phugoid.t, y[0].T[0], label="u")
-plt.plot(phugoid.t, y[0].T[1], label="alpha")
-plt.plot(phugoid.t, y[0].T[2], label="theta")
+# plt.plot(phugoid.t, y[0].T[0], label="u")
+# plt.plot(phugoid.t, y[0].T[1], label="alpha")
+# plt.plot(phugoid.t, y[0].T[2], label="theta")
+plt.plot(phugoid.t, phugoid.qplot)
 plt.plot(phugoid.t, y[0].T[3], label="q")
 plt.legend()
 plt.subplot(122)
-plt.title("flight data")
-plt.plot(phugoid.t, phugoid.totalplot.T)
+plt.plot(phugoid.t, phugoid.deplot)
+# plt.title("flight data")
+
 plt.show()
